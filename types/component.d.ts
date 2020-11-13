28e398f289bfc38094ab584c02eaee5a062091e0
yelingfeng <yelingfeng521@gmail.com>
2020-11-13 10:27:56 +0800

    init

--- types/component.d.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-/** PandoraUI component common definition */
-export declare class PandoraUIComponent extends Vue {
-  /** Install component into Vue */
-  static install(vue: typeof Vue): void
-}
--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './pandora'
-
-import * as PandoraUI from './pandora'
-export default PandoraUI
