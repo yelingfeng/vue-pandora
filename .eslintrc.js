aea88fa26fcea94dd0647067a476b0079f2738e0
yelingfeng <yelingfeng521@gmail.com>
2021-04-28 10:45:32 +0800

    build(change): update webpack build lib

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-rest-params': 0,
     'import/no-unresolved': 0,
     'import/prefer-default-export': 0,
     'import/no-extraneous-dependencies': 0,
