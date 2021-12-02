66fac691a8009f0ce3873086f4e4e9ecf7224e3c
yelingfeng <yelingfeng521@gmail.com>
2021-12-02 09:14:05 +0800

    ci(vueconfig): add terser drop console

--- vue.config.js
+++ vue.config.js
@@ -36,6 +36,19 @@ module.exports = {
     // https://webpack.js.org/configuration/devtool/#development
     config.when(!IS_PROD, config => config.devtool('cheap-eval-source-map'))
 
+    if (IS_PROD) {
+      config.optimization.minimizer('terser').tap(args => {
+        Object.assign(args[0].terserOptions.compress, {
+          // 生产模式 console.log 去除
+          // warnings: false , // 默认 false
+          // drop_console:  ,
+          drop_console: true,
+          drop_debugger: true, // 默认也是true
+          pure_funcs: ['console.log']
+        })
+        return args
+      })
+    }
     // remove vue-cli-service's progress output
     config.plugins.delete('progress')
   }
